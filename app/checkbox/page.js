5a695373068d81935989a0aab9edf8cf8a5e490f
D.A. Kahn <kahn.da@heb.com>
2025-05-19 11:07:42 -0500

    Fix unescaped character

--- app/checkbox/page.js
+++ app/checkbox/page.js
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import styles from './styles.module.css'
-import { useState } from 'react';
+import styles from "./styles.module.css";
+import { useState } from "react";
 
 export default function Page() {
-
   const [communicationPreferences, setCommunicationPreferences] = useState({
     textMessages: false,
     emails: false,
   });
 
-  const allSelected = communicationPreferences.textMessages && communicationPreferences.emails;
+  const allSelected =
+    communicationPreferences.textMessages && communicationPreferences.emails;
 
   const handleSelectAll = (element) => {
     const checked = element.target.checked;
@@ -19,7 +19,7 @@ export default function Page() {
       textMessages: checked,
       emails: checked,
     });
-  }
+  };
 
   const handleOptionChange = (element) => {
     const { name, checked } = element.target;
@@ -27,7 +27,7 @@ export default function Page() {
       ...prevOptions,
       [name]: checked,
     }));
-  }
+  };
 
   return (
     <div>
@@ -38,60 +38,148 @@ export default function Page() {
           <li>
             <h2>Default</h2>
             <div>
-              <input id="base-checkbox" className={styles.checkbox} aria-invalid="false" type="checkbox" />
-              <label htmlFor="base-checkbox" className={styles.checkbox__label}>Email receipt</label>
+              <input
+                id="base-checkbox"
+                className={styles.checkbox}
+                aria-invalid="false"
+                type="checkbox"
+              />
+              <label htmlFor="base-checkbox" className={styles.checkbox__label}>
+                Email receipt
+              </label>
             </div>
           </li>
           <li>
             <h2>Disabled</h2>
             <div>
-              <input id="base-checkbox" aria-disabled="true" disabled className={styles.checkbox} aria-invalid="false" type="checkbox" />
-              <label htmlFor="base-checkbox" className={styles.checkbox__label}>Email receipt</label>
+              <input
+                id="base-checkbox"
+                aria-disabled="true"
+                disabled
+                className={styles.checkbox}
+                aria-invalid="false"
+                type="checkbox"
+              />
+              <label htmlFor="base-checkbox" className={styles.checkbox__label}>
+                Email receipt
+              </label>
             </div>
-
           </li>
           <li>
             <h2>Hint</h2>
             <div>
-              <input id="base-checkbox" aria-describedby="hint-checkbox" className={styles.checkbox} aria-invalid="false" type="checkbox" />
-              <label htmlFor="base-checkbox" className={styles.checkbox__label}>Email receipt</label>
-              <div className={styles.hinttext}>User's should expect a minimum of 10 emails a day</div>
+              <input
+                id="base-checkbox"
+                aria-describedby="hint-checkbox"
+                className={styles.checkbox}
+                aria-invalid="false"
+                type="checkbox"
+              />
+              <label htmlFor="base-checkbox" className={styles.checkbox__label}>
+                Email receipt
+              </label>
+              <div className={styles.hinttext}>
+                Users should expect a minimum of 10 emails a day
+              </div>
             </div>
           </li>
           <li>
             <h2>Error</h2>
             <div>
-              <input id="base-checkbox" className={styles.checkbox} aria-invalid="true" aria-describedby="checkbox__errormessage" type="checkbox" />
-              <label htmlFor="base-checkbox" className={styles.checkbox__label}>Email receipt</label>
+              <input
+                id="base-checkbox"
+                className={styles.checkbox}
+                aria-invalid="true"
+                aria-describedby="checkbox__errormessage"
+                type="checkbox"
+              />
+              <label htmlFor="base-checkbox" className={styles.checkbox__label}>
+                Email receipt
+              </label>
               <div className={styles.errortext__wrapper}>
-                <span className={styles.erroricon} aria-label="error" role="img">⚠</span>
-                <div id="checkbox__errormessage" className={styles.errortext}>You must agree to receive emails!</div>
+                <span
+                  className={styles.erroricon}
+                  aria-label="error"
+                  role="img"
+                >
+                  ⚠
+                </span>
+                <div id="checkbox__errormessage" className={styles.errortext}>
+                  You must agree to receive emails!
+                </div>
               </div>
             </div>
           </li>
           <li>
             <h2>Indeterminate</h2>
             <fieldset>
-              <legend className={styles.legend}>Communication preferences</legend>
+              <legend className={styles.legend}>
+                Communication preferences
+              </legend>
               <div className={styles.checkbox__container}>
-                <input checked={allSelected} onChange={handleSelectAll} aria-describedby="indeterminate-checkbox-hinttext" id="base-checkbox" className={styles.checkbox} aria-invalid="false" type="checkbox" />
-                <label htmlFor="base-checkbox" className={styles.checkbox__label}>Select all</label>
+                <input
+                  checked={allSelected}
+                  onChange={handleSelectAll}
+                  aria-describedby="indeterminate-checkbox-hinttext"
+                  id="base-checkbox"
+                  className={styles.checkbox}
+                  aria-invalid="false"
+                  type="checkbox"
+                />
+                <label
+                  htmlFor="base-checkbox"
+                  className={styles.checkbox__label}
+                >
+                  Select all
+                </label>
               </div>
               <div>
                 <div className={styles.checkbox__nestedcontainer}>
-                  <input name="textMessages" checked={communicationPreferences.textMessages} onChange={handleOptionChange} id="nested-checkbox1" className={styles.checkbox} aria-invalid="false" type="checkbox" />
-                  <label htmlFor="nested-checkbox1" className={styles.checkbox__label}>Text messages</label>
+                  <input
+                    name="textMessages"
+                    checked={communicationPreferences.textMessages}
+                    onChange={handleOptionChange}
+                    id="nested-checkbox1"
+                    className={styles.checkbox}
+                    aria-invalid="false"
+                    type="checkbox"
+                  />
+                  <label
+                    htmlFor="nested-checkbox1"
+                    className={styles.checkbox__label}
+                  >
+                    Text messages
+                  </label>
                 </div>
                 <div className={styles.checkbox__nestedcontainer}>
-                  <input name="emails" checked={communicationPreferences.emails} onChange={handleOptionChange} id="nested-checkbox2" className={styles.checkbox} aria-invalid="false" type="checkbox" />
-                  <label htmlFor="nested-checkbox2" className={styles.checkbox__label}>Emails</label>
+                  <input
+                    name="emails"
+                    checked={communicationPreferences.emails}
+                    onChange={handleOptionChange}
+                    id="nested-checkbox2"
+                    className={styles.checkbox}
+                    aria-invalid="false"
+                    type="checkbox"
+                  />
+                  <label
+                    htmlFor="nested-checkbox2"
+                    className={styles.checkbox__label}
+                  >
+                    Emails
+                  </label>
                 </div>
               </div>
-              <div className={styles.hinttext} id="indeterminate-checkbox-hinttext">We highly advise you to agree to one or both modes of communication.</div>
+              <div
+                className={styles.hinttext}
+                id="indeterminate-checkbox-hinttext"
+              >
+                We highly advise you to agree to one or both modes of
+                communication.
+              </div>
             </fieldset>
           </li>
         </ul>
       </main>
     </div>
-  )
+  );
 }
